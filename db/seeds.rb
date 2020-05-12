ed3585ee0fdb547d700fac4edcf7fc2004a0194c
fleatama <fleatama@gmail.com>
2020-05-12 19:41:57 +0900

    change seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%W[コーヒ コーラ お茶 紅茶 ミルクティ 水].each { |a| Drink.create(name: a) }
